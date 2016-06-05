6c775b3cc540104efce2f6512c2cfa9ed697d41c
transplanar <transplanar@gmail.com>
2016-06-05 17:43:57 -0400

    Displaying page with choices

--- app/scripts/controllers/StoryNavCtrl.js
+++ app/scripts/controllers/StoryNavCtrl.js
@@ -10,9 +10,26 @@
       pages: []
     };
     
-    $scope.getPageID = function(parentPage){
-      console.log('getting id for', parentPage);
+    var Page = function(args){
+      this.parentPage = args.parentPage;
+      this.title = args.title;
+      this.summary = args.summary;
+      this.content = args.content;
+      this.choices = [];
+      this.endpoint = args.endpoint;
       
+      this.init = function(){
+        this.id = $scope.getPageID(this.parentPage);
+
+        for(var i = 0; i < args.choices.length; i++){
+          this.choices.push(new Choice(args.choices[i].text, this));    
+        }
+      }
+        
+      this.init();
+    };
+    
+    $scope.getPageID = function(parentPage){
       if(parentPage===null){
         return 1;
       }else{
@@ -26,68 +43,50 @@
       }
     }
     
-    function Choice(text, parent){
-      return {text: text, dest: $scope.getPageID(parent)};
+    function Choice(text, parentPage){
+      return {text: text, dest: $scope.getPageID(parentPage)};
     }
     
     $scope.testStory.pages.push(
-      {
-        parentPage: null,
-        id: $scope.getPageID(this.parentPage),
-        title: 'Initial page', //String
-        summary: 'First page', //String
-        content: 'You see two doors. WHich do you choose?', //String
-        choices: [
-                        new Choice('Left door', this),
-                        new Choice('Right door', this)
-                       ],
-        endpoint: false, //Boolean
-      }
+      new Page(
+        {
+          parentPage: null,
+          title: 'Initial page', //String
+          summary: 'First page', //String
+          content: 'You see two doors. Which do you choose?', //String
+          choices: [
+                        {text: 'Left door', parentPage: this},
+                        {text: 'Right door', parentPage: this}
+                   ],
+          endpoint: false //Boolean
+        }
+      )
     );
     
     $scope.testStory.pages.push(
-      {
-        parentPage: null,
-        id: $scope.getPageID(this.parentPage),
-        title: 'Initial page 2', //String
-        summary: 'First page', //String
-        content: 'You see two doors. WHich do you choose?', //String
-        choices: [
-          new Choice('Left door', this),
-          new Choice('Right door', this)
-        ],
-        endpoint: false, //Boolean
-      }
+      new Page(
+        {
+          parentPage: null,
+          title: 'Left door', //String
+          summary: 'First page', //String
+          content: 'You entered the left door.', //String
+          choices: [ ],
+          endpoint: true //Boolean
+        }
+      )
     );
     
     $scope.testStory.pages.push(
-      {
-        parentPage: null,
-        id: $scope.getPageID(this.parentPage),
-        title: 'Initial page 3', //String
-        summary: 'First page', //String
-        content: 'You see two doors. WHich do you choose?', //String
-        choices: [
-          new Choice('Left door', this),
-          new Choice('Right door', this)
-        ],
-        endpoint: false, //Boolean
-      }
-    );
-    
-    $scope.testStory.pages.push(
-      {
-        parentPage: null,
-        id: $scope.getPageID(this.parentPage),
-        title: 'Initial page 4', //String
-        summary: 'First page', //String
-        content: 'You see two doors. WHich do you choose?', //String
-        choices: [
-          new Choice('Left door', this),
-          new Choice('Right door', this)
-        ],
-        endpoint: false, //Boolean
-      }
+      new Page(
+        {
+          parentPage: null,
+          title: 'Right door', //String
+          summary: 'First page', //String
+          content: 'You entered the right door.', //String
+          choices: [ ],
+          endpoint: true //Boolean
+        }
+      )
     );
     
     var getPageByID = function(pages, id){
