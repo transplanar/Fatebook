53617b2c54fe2eaf588321dfabac05316356cf2e
transplanar <transplanar@gmail.com>
2016-06-07 19:11:06 -0400

    Move proper code into StoryNavSrv service

--- app/scripts/controllers/StoryNavCtrl.js
+++ app/scripts/controllers/StoryNavCtrl.js
@@ -1,123 +1,19 @@
 (function(){
-  function StoryNavCtrl($scope){
-    var testStory = {
-      title: 'TestStory Title',
-      description: 'Test of story nav system.',
-      summary: 'First story to test story nav system',
-      keywords: ['test'],
-      system: 'default',
-      rating: 'unrated', //TODO set this as default in Rails
-      pages: []
-    };
+//  function StoryNavCtrl($scope){
+  function StoryNavCtrl($scope, StoryNavSrv){
+    StoryNavSrv.initializeStoryData();
     
-    $scope.currentStory = testStory;
-    
-    //NOTE Need to have choices properly generate their ID
-    var Page = function(args){
-      this.parentPage = args.parentPage;
-      this.id = args.id;
-      this.title = args.title;
-      this.summary = args.summary;
-      this.content = args.content;
-      this.choices = args.choices; //Displayed to user
-      this.branches = []; //Ref to other pages
-      this.endpoint = args.endpoint;
-      
-      this.init = function(){
-        if(this.parentPage){
-          this.parentPage.branches.push(this);
-        }
-      }
-    };
-    
-    var generateBranchIDs = function(){
-      var pages = $scope.currentStory.pages;
-
-      pages[0].id = '1';
-      
-      for(var i = 1; i < pages.length; i++){
-          if(pages[i].parentPage){
-            var lastChar = pages[i].parentPage.id[pages[i].parentPage.id.length -1];
-            var childIndex = pages[i].parentPage.branches.indexOf(pages[i]);
-            
-            var reg = /^\d+$/;
-            
-            if(reg.test(lastChar)){
-              pages[i].id = pages[i].parentPage.id + indexToAlpha(childIndex);
-            }else{
-              pages[i].id = pages[i].parentPage.id + (childIndex+1);
-            }
-        }
-      }
-    }
-      
-    var indexToAlpha = function(index){
-      return String.fromCharCode(index + 65);
-    }
-    
-    $scope.currentStory.pages.push(
-      new Page(
-        {
-          parentPage: null,
-          title: 'Initial page', //String
-          summary: 'First page', //String
-          content: 'You see two doors. Which do you choose?', //String
-          choices: [
-                        {text: 'Left door', dest: '1A'},
-                        {text: 'Right door', dest: '1B'}
-                   ],
-          endpoint: false //Boolean
-        }
-      )
-    );
-    
-    $scope.currentPage = $scope.currentStory.pages[0];
-    
-    $scope.currentStory.pages.push(
-      new Page(
-        {
-          parentPage: $scope.currentStory.pages[0],
-          title: 'Left door', //String
-          summary: 'First page', //String
-          content: 'You entered the left door.', //String
-          choices: [
-            {text: 'Something else', dest: '1A1'}
-          ],
-          endpoint: true //Boolean
-        }
-      )
-    );
-    
-    $scope.currentStory.pages.push(
-      new Page(
-        {
-          parentPage: $scope.currentStory.pages[0],
-          title: 'Right door', //String
-          summary: 'First page', //String
-          content: 'You entered the right door.', //String
-          endpoint: true //Boolean
-        }
-      )
-    );
-    
-    //TODO Get page by ID for branching?
-    $scope.currentStory.pages.push(
-      new Page(
-        {
-          parentPage: $scope.currentStory.pages[1],
-          title: 'Super last', //String
-          summary: 'First page', //String
-          content: 'Even more final.', //String
-          endpoint: true //Boolean
-        }
-      )
-    );
+    $scope.currentStory = StoryNavSrv.currentStory;
+    $scope.currentPage = StoryNavSrv.currentPage;
     
     $scope.setPage = function(dest){
-      var pages = $scope.currentStory.pages;
+      console.log('setting page');
+      var pages = StoryNavSrv.currentStory.pages;
       
       for(var i in pages){
         if (pages[i].id === dest){
+          //NOTE Better way to sync controller and service?
+          StoryNavSrv.currentPage = pages[i];
           $scope.currentPage = pages[i];
         }
       }
@@ -125,26 +21,17 @@
       return null;
     }
     
-    var initializePages = function(){
-      for (var i = 0; i < $scope.currentStory.pages.length; i++){
-        $scope.currentStory.pages[i].init();
-      }
-    }
-    
-    $scope.$on('$viewContentLoaded', function(){
-      initializePages();
-      generateBranchIDs();
-      
-      var pages = $scope.currentStory.pages;
-    });
-    
     angular.element(document).bind('keyup', function (e) {
-      if($scope.currentPage && $scope.currentPage.choices){
+      if(StoryNavSrv.currentPage && StoryNavSrv.currentPage.choices){
         var choiceIndex = e.keyCode - 49;
-        var choice = $scope.currentPage.choices[choiceIndex];
+        var choice = StoryNavSrv.currentPage.choices[choiceIndex];
+        
+        console.log('choice', choice);
         
         if(choice){
+          //$scope or StoryNavSrv here?
           $scope.$apply($scope.setPage(choice.dest));
+//          $scope.$apply(StoryNavSrv.setPage(choice.dest));
         }
       }
     });
@@ -152,5 +39,6 @@
   
   angular
     .module('fatebook')
-    .controller('StoryNavCtrl', ['$scope', StoryNavCtrl]);
+//    .controller('StoryNavCtrl', ['$scope', StoryNavCtrl]);
+    .controller('StoryNavCtrl', ['$scope', 'StoryNavSrv', StoryNavCtrl]);
 })();
--- app/scripts/services/StoryNavSrv.js
+++ app/scripts/services/StoryNavSrv.js
@@ -1,9 +1,189 @@
-//(function(){
-//  function StoryNavSrv{
-//    
-//  }
-//  
-//  angular
-//    .module('fatebook')
-//    .factory('StoryNavSrv', [StoryNavSrv])
-//})():
+(function(){
+  function StoryNavSrv() {
+    var StoryNavSrv = {};
+
+
+    //NOTE Need to have choices properly generate their ID
+    var Page = function(args){
+      this.parentPage = args.parentPage;
+      this.id = args.id;
+      this.title = args.title;
+      this.summary = args.summary;
+      this.content = args.content;
+      this.choices = args.choices; //Displayed to user
+      this.branches = []; //Ref to other pages
+      this.endpoint = args.endpoint;
+
+      this.init = function(){
+        if(this.parentPage){
+          
+          this.parentPage.branches.push(this);
+        }
+      }
+    };
+
+    var generateBranchIDs = function(){
+      var pages = StoryNavSrv.currentStory.pages;
+
+      pages[0].id = '1';
+
+      for(var i = 1; i < pages.length; i++){
+        if(pages[i].parentPage){
+          var lastChar = pages[i].parentPage.id[pages[i].parentPage.id.length -1];
+          var childIndex = pages[i].parentPage.branches.indexOf(pages[i]);
+
+          var reg = /^\d+$/;
+
+          if(reg.test(lastChar)){
+            pages[i].id = pages[i].parentPage.id + indexToAlpha(childIndex);
+          }else{
+            pages[i].id = pages[i].parentPage.id + (childIndex+1);
+          }
+        }
+      }
+    }
+
+    var indexToAlpha = function(index){
+      return String.fromCharCode(index + 65);
+    }
+    
+    var initializePages = function(){
+      for (var i = 0; i < StoryNavSrv.currentStory.pages.length; i++){
+        StoryNavSrv.currentStory.pages[i].init();
+      }
+    }
+    
+    var getPageFromID = function(id){
+      console.log('getting page id');
+      var pages = StoryNavSrv.currentStory.pages;
+      
+      console.log('args',id);
+      
+      for(var i in pages){
+        console.log('comparing',pages[i].id,id);
+        if(pages[i].id === id){
+          return pages[i];
+        }
+      }
+    
+      return undefined;
+    };
+
+//    StoryNavSrv.$on('$viewContentLoaded', function(){
+    StoryNavSrv.initializeStoryData = function(){
+      initializePages();
+      generateBranchIDs();
+
+//      var pages = StoryNavSrv.currentStory.pages;
+    };
+    
+//    TEST FUNCTIONS/DATA ****************************************
+    var testStory = {
+      title: 'TestStory Title',
+      description: 'Test of story nav system.',
+      summary: 'First story to test story nav system',
+      keywords: ['test'],
+      system: 'default',
+      rating: 'unrated', //TODO set this as default in Rails
+      pages: []
+    };
+
+    StoryNavSrv.currentStory = testStory;
+    
+    StoryNavSrv.currentStory.pages.push(
+      new Page(
+        {
+          parentPage: null,
+          title: 'Initial page', //String
+          summary: 'First page', //String
+          content: 'You see two doors. Which do you choose?', //String
+          choices: [
+            {text: 'Left door', dest: '1A'},
+            {text: 'Right door', dest: '1B'}
+          ],
+          endpoint: false //Boolean
+        }
+      )
+    );
+
+    StoryNavSrv.currentPage = StoryNavSrv.currentStory.pages[0];
+
+    StoryNavSrv.currentStory.pages.push(
+      new Page(
+        {
+          parentPage: StoryNavSrv.currentStory.pages[0],
+          title: 'Left door', //String
+          summary: 'First page', //String
+          content: 'You entered the left door.', //String
+          choices: [
+            {text: 'Something else', dest: '1A1'}
+          ],
+          endpoint: true //Boolean
+        }
+      )
+    );
+
+    StoryNavSrv.currentStory.pages.push(
+      new Page(
+        {
+          parentPage: StoryNavSrv.currentStory.pages[0],
+          title: 'Right door', //String
+          summary: 'First page', //String
+          content: 'You entered the right door.', //String
+          endpoint: true //Boolean
+        }
+      )
+    );
+
+    //TODO Get page by ID for branching?
+    StoryNavSrv.currentStory.pages.push(
+      new Page(
+        {
+          parentPage: StoryNavSrv.currentStory.pages[1],
+          title: 'Super last', //String
+          summary: 'First page', //String
+          content: 'Even more final.', //String
+          choices: [
+            {text: 'This is the end?', dest: '1A1A'},
+            {text: 'This is the beginnging', dest: '1A1B'}
+          ],
+          endpoint: true //Boolean
+        }
+      )
+    );
+    
+    //TODO Get page by ID for branching?
+    StoryNavSrv.currentStory.pages.push(
+      new Page(
+        {
+          parentPage: getPageFromID('1B1'),
+//          parentPage: StoryNavSrv.currentStory.pages[2],
+          title: 'Super last', //String
+          summary: 'First page', //String
+          content: 'It is indeed the end.', //String
+          endpoint: true //Boolean
+        }
+      )
+    );
+    
+    //TODO Get page by ID for branching?
+    StoryNavSrv.currentStory.pages.push(
+      new Page(
+        {
+          parentPage: getPageFromID('1B1'),
+//          parentPage: StoryNavSrv.currentStory.pages[2],
+          title: 'Super last', //String
+          summary: 'First page', //String
+          content: 'Nah jk lol.', //String
+          endpoint: true //Boolean
+        }
+      )
+    );
+
+    return StoryNavSrv;
+  }
+  
+  angular
+    .module('fatebook')
+    .factory('StoryNavSrv', [StoryNavSrv])
+})();
