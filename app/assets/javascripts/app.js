953d1126e59c34fd0ca58e198a57a38583d61f7f
transplanar <transplanar@gmail.com>
2016-06-12 16:07:41 -0400

    Attempt to install angular-rails-templates gem

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,6 @@
 //= require angular
 //= require angular-resource
+//= require angular-ui-router/release/angular-ui-router
 
 //NOTE include "use strict"?
 
@@ -15,7 +16,8 @@
       .state('landing',{
       url:'/stories',
       controller: 'StoryNavCtrl',
-      templateUrl: 'templates/story.html'
+      // templateUrl: 'templates/story.html'
+      templateUrl: 'stories/test.html'
       // templateUrl: '/views/stories/index.html.erb'
     });
   };
