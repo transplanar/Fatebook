11ccb568812309cf06f295852e22ae1ec560b336
transplanar <transplanar@gmail.com>
2016-08-17 22:10:56 -0400

    Tweaking package.json for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :pages
   resources :stories
-  # resources :users, only: [:new, :show, :create, :update, :destroy]
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :branches, only: [:index, :destroy]
@@ -15,7 +14,4 @@ Rails.application.routes.draw do
   root 'landing#index'
 
   get "*unmatched_route" => 'landing#index'
-
-  # NOTE more correct than landing?
-  # root to: 'application#angular'
 end
